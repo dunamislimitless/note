6c619ac6c80cf3dbb9f01a856a8a5be0a8a0f33f
dunamislimitless <ogundojusamuel@gmail.com>
2022-12-01 18:09:26 +0100

    damm

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes/views/login_view.dart';
+import 'package:notes/views/register.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -9,69 +10,18 @@ void main() {
       backgroundColor: Colors.white38,
       primarySwatch: Colors.green,
     ),
-    home: const LoginPage(),
+    home: const HomePage(),
   ));
 }
 
-class Register extends StatefulWidget {
-  const Register({Key? key}) : super(key: key);
-
-  @override
-  State<Register> createState() => _RegisterState();
-}
-
-class _RegisterState extends State<Register> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-
-    super.dispose();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextField(
-                controller: _email,
-                enableSuggestions: false,
-                autocorrect: false,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  hintText: 'Email',
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(
-                hintText: 'Password',
-              ),
-            ),
-          ),
-          TextButton(onPressed: () async {}, child: const Text('Register')),
-        ],
+        title: Text('Home Page'),
       ),
     );
   }
