c0eaeb67b2ab7694e44ea49fae4de2b566808b91
dunamislimitless <ogundojusamuel@gmail.com>
2022-12-09 17:47:32 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,17 @@ import 'package:notes/views/register.dart';
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'My note',
-    theme: ThemeData(
-      backgroundColor: Colors.white38,
-      primarySwatch: Colors.green,
-    ),
-    home: const Register(),
-  ));
+      debugShowCheckedModeBanner: false,
+      title: 'My note',
+      theme: ThemeData(
+        backgroundColor: Colors.white38,
+        primarySwatch: Colors.green,
+      ),
+      home: const Register(),
+      routes: {
+        '/login/': (context) => const LoginPage(),
+        'register/': (context) => const Register(),
+      }));
 }
 
 // class HomePage extends StatelessWidget {
